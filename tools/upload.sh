5609e9b44a1a64c9909ba653d077f732ccee88c3
Jason P. Rahman <rahmanj@purdue.edu>
2013-08-08 23:13:13 -0500

    Fixed paths for upload

--- tools/upload.sh
+++ tools/upload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Uploading theme..."
-scp -r ~/Dropbox/CRU_Website/wp-theme/* rahmanj@ftp.ics.purdue.edu:www/wp-content/themes/PurdueCru
-sudo cp -r ~/Dropbox/CRU_Website/wp-theme/* /var/www/wp-content/themes/PurdueCru
+scp -r ~/Dropbox/CRU_Website/wp-theme/* rahmanj@ftp.ics.purdue.edu:www/wp-content/themes/PurdueCRU
+sudo cp -r ~/Dropbox/CRU_Website/wp-theme/* /var/www/wp-content/themes/PurdueCRU
 
 echo "Uploading plugin..."
-scp -r ~/Dropbox/CRU_Website/wp-plugin/PurdueCRU rahmanj@ftp.ics.purdue.edu:www/wp-content/plugins/PurdueCru
-sudo cp -r ~/Dropbox/CRU_Website/wp-plugin/PurdueCRU /var/www/wp-content/plugins/PurdueCru
+scp -r ~/Dropbox/CRU_Website/wp-plugin/* rahmanj@ftp.ics.purdue.edu:www/wp-content/plugins/PurdueCRU
+sudo cp -r ~/Dropbox/CRU_Website/wp-plugin/* /var/www/wp-content/plugins/PurdueCRU
