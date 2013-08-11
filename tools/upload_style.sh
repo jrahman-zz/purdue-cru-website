bf83b326b2e972b872e261e2254a3dfb8bda0a9b
Jason P. Rahman <rahmanj@purdue.edu>
2013-08-10 22:26:05 -0500

    Add script to upload

--- tools/upload_style.sh
+++ tools/upload_style.sh
@@ -2,5 +2,9 @@
 
 echo "Uploading stylesheet..."
 scp -r ~/Dropbox/CRU_Website/wp-theme/PurdueCruTheme/style.css rahmanj@ftp.ics.purdue.edu:www/wp-content/themes/PurdueCRU
+
+scp -r ~/Dropbox/CRU_Website/wp-theme/PurdueCruTheme/js/site.js rahmanj@ftp.ics.purdue.edu:www/wp-content/themes/PurdueCRU/js
+
 sudo cp -r ~/Dropbox/CRU_Website/wp-theme/PurdueCruTheme/style.css /var/www/wp-content/themes/PurdueCRU
+sudo cp -r ~/Dropbox/CRU_Website/wp-theme/PurdueCruTheme/js/site.js /var/www/wp-content/themes/PurdueCRU/js
 
