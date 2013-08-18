3dcab3c40bd837948c3236b4281c3a49d3d63bd5
Jason P. Rahman <rahmanj@purdue.edu>
2013-08-18 04:26:45 -0500

    Remove old installation code, add uninstalltion support

--- wp-plugin/PurdueCruPlugin/CRU_Plugin.php
+++ wp-plugin/PurdueCruPlugin/CRU_Plugin.php
@@ -120,87 +120,19 @@ class CRU_Plugin {
         }
     }
 
-
-    /**
-     *
-     * Run a query through dbDelta to update/create the database
-     *
-     */
-    public function cru_install_database() {
-        global $wpdb;
-        global $cru_db_version;
-
-        $target_areas_table = $wpdb->prefix . CRU_Utils::_target_areas_table;
-        $small_groups_table = $wpdb->prefix . CRU_Utils::_small_groups_table;
-        $area_contacts_table = $wpdb->prefix . CRU_Utils::_area_contacts_table;
-        $providers_table = $wpdb->prefix . CRU_Utils::_provider_table;
-        
-        // Define SQL to create database tables
-        // Presently dbDelta throws a hissy fit over the foreign keys
-        $sql = "CREATE TABLE $target_areas_table (
-        area_id mediumint(9) NOT NULL AUTO_INCREMENT,
-        area_name char(64) NOT NULL,
-        PRIMARY KEY  (area_id)
-        );
-        CREATE TABLE $small_groups_table (
-        group_id mediumint(9) NOT NULL AUTO_INCREMENT,
-        area_id mediumint(9) NOT NULL,
-        contact_id bigint(20) unsigned NOT NULL,
-        day char(10) NOT NULL,
-        time char(10) NOT NULL,
-        location varchar(512) NOT NULL,
-        men tinyint(2) NOT NULL,
-        women tinyint(2) NOT NULL,
-        PRIMARY KEY  (group_id)," .
-        /*FOREIGN KEY  (area_id) REFERENCES $target_areas_table(area_id),
-        FOREIGN KEY  (contact_id) REFERENCES $wpdb->users(ID)*/
-        ");
-        CREATE TABLE $area_contacts_table (
-        area_id mediumint(9) NOT NULL,
-        contact_id bigint(20) unsigned NOT NULL,
-        affiliation_type tinyint(3) NOT NULL," .
-        /*FOREIGN KEY  (area_id) REFERENCES $target_area_table(area_id),
-        FOREIGN KEY  (contact_id) REFERENCES $wpdb->users(ID),*/
-        "PRIMARY KEY  (area_id,contact_id,affiliation_type)
-        );
-        CREATE TABLE $providers_table (
-        provider_id tinyint(5) NOT NULL AUTO_INCREMENT,
-        provider_name char(50) NOT NULL,
-        provider_gateway char(125) NOT NULL,
-        PRIMARY KEY  (provider_id)
-        );";
-            
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-
-        dbDelta($sql);
-
-        // Populate the providers table with a pre-selected list of providers
-        $wpdb->insert($providers_table,
-            array('provider_id' => 1, 'provider_name' => "AT&T", 'provider_gateway' => 'txt.att.net'));
-        $wpdb->insert($providers_table,
-            array('provider_id' => 2, 'provider_name' => "Verizon", 'provider_gateway' => 'vtext.com'));
-        $wpdb->insert($providers_table,
-            array('provider_id' => 3, 'provider_name' => "Sprint", 'provider_gateway' => "messaging.sprintpcs.com"));
-        $wpdb->insert($providers_table,
-            array('provider_id' => 4, 'provider_name' => "T-Mobile", 'provider_gateway' => "tmomail.net"));
-        $wpdb->insert($providers_table,
-            array('provider_id' => 5, 'provider_name' => "Straight Talk", 'provider_gateway' => "vtext.com"));
-        $wpdb->insert($providers_table,
-            array('provider_id' => 6, 'provider_name' => "TracFone", 'provider_gateway' => "mmst5.tracfone.com"));
-        
-        add_option("cru_db_version", $cru_db_version);
-    } // public function cru_install_database()
-
-
-    // TODO Consider implementing this
-    
     /** 
      *
-     *
+     * Run module uninstallation hooks
      *
      */
     public function cru_uninstall_plugin() {
-
+        if (is_array($this->modules)) {
+            foreach ($this->modules as $module) {
+                if (method_exists($module, 'uninstall_module')) {
+                    $module->uninstall_module();
+                }
+            }
+        }
     }
  
     /**
