00adedf7c79e99ae9aa10e9bee955d20d8b5b95f
Jason P. Rahman <rahmanj@purdue.edu>
2014-01-12 13:20:24 -0600

    Updated the upload tool script

--- tools/upload.sh
+++ tools/upload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Uploading theme..."
-scp -r ~/Dropbox/CRU_Website/wp-theme/PurdueCruTheme/* rahmanj@ftp.ics.purdue.edu:www/wp-content/themes/PurdueCruTheme
+scp -r ~/Dropbox/CRU_Website/wp-theme/PurdueCruTheme/* rahmanj@mace.itap.purdue.edu:www/wp-content/themes/PurdueCruTheme
 sudo cp -r ~/Dropbox/CRU_Website/wp-theme/PurdueCruTheme/* /var/www/wp-content/themes/PurdueCruTheme
 
 echo "Uploading plugin..."
-scp -r ~/Dropbox/CRU_Website/wp-plugin/PurdueCruPlugin/* rahmanj@ftp.ics.purdue.edu:www/wp-content/plugins/PurdueCruPlugin
+scp -r ~/Dropbox/CRU_Website/wp-plugin/PurdueCruPlugin/* rahmanj@mace.itap.purdue.edu:www/wp-content/plugins/PurdueCruPlugin
 sudo cp -r ~/Dropbox/CRU_Website/wp-plugin/PurdueCruPlugin/* /var/www/wp-content/plugins/PurdueCruPlugin
